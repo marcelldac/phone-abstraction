16c9db53cec1268118bd1f47e982015a1ac35536
marcelldac <dacmarcell@gmail.com>
2024-01-18 22:13:35 -0300

    feat: smartphone base class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace App
+{
+    public abstract class SmartPhone
+    {
+        string Numero { get; set; }
+        string Modelo { get; set; }
+        string IMEI { get; set; }
+        int Memoria { get; set; }
+
+        protected SmartPhone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void CallToAnyone()
+        {
+        }
+
+        public void ReceiveCall()
+        {
+        }
+
+        public abstract void installApplication(string app);
+    }
+}
