2f4a45c3ec4daed2d6ed56a52888df00c9fb7f1f
marcelldac <dacmarcell@gmail.com>
2024-01-18 22:15:01 -0300

    feat: iphone

--- Program.cs
+++ Program.cs
@@ -34,7 +34,19 @@
 
         public override void installApplication(string app)
         {
-            Console.WriteLine($"Installing application: {app} no Nokia");
+            Console.WriteLine($"Installing application: {app} on Nokia");
+        }
+    }
+
+    public class Iphone : SmartPhone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void installApplication(string app)
+        {
+            Console.WriteLine($"Installing application: {app} on Iphone");
         }
     }
 }
