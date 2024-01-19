e915912775a500a2b15a81748c1ca8ac8df37e79
marcelldac <dacmarcell@gmail.com>
2024-01-18 22:28:11 -0300

    feat: using features

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using App;
+
+SmartPhone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+
+nokia.CallToAnyone();
+nokia.ReceiveCall();
+nokia.installApplication("Github");
+
+SmartPhone iphone = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+
+iphone.CallToAnyone();
+iphone.ReceiveCall();
+iphone.installApplication("Github");
